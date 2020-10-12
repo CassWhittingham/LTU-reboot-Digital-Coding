2fd8871d1d06349506720391e6992b829e439008
CassWhittingham <72015400+CassWhittingham@users.noreply.github.com>
2020-10-12 11:34:46 +0100

    add javascript file and create 3.1.4

---
+++ module-3/scripts/main.js
@@ -0,0 +1,15 @@
+
+var numberOfSmarties = 40;
+var numberOfIceCreams = 30;
+
+var total = numberOfSmarties + numberOfIceCreams; //adding two values together. should output 70
+
+console.log( total ); //webpage should show 70
+
+/*
++ add
+- subtract
+/ divide
+* multiply
+% modulus
+*/
